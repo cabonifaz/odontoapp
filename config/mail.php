0d7070562ec77ecd479a3294f50b0742f46bfe38
geeky.ciudapolis@gmail.com <geeky.ciudapolis@gmail.com>
2025-11-23 12:57:07 -0500

    mail

--- config/mail.php
+++ config/mail.php
@@ -43,6 +43,15 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
+
+            // AGREGAR ESTE BLOQUE PARA SOLUCIONAR TIMEOUTS EN RAILWAY
+            'stream' => [
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ],
         ],
 
         'ses' => [
